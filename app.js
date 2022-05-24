8868bc0e2ecefa890719be7655556018d5138f10
mason <masonjar@ihuiseong-ui-MacBookPro.local>
2022-05-24 16:34:51 +0900

    FIX: app.js using aws-sdk

--- app.js
+++ app.js
@@ -3,30 +3,33 @@ const app = express()
 const cors = require('cors');
 const { Producer } = require('sqs-producer');
 const port = 3000
-const bodyParser = require('body-parser')
-
-const producer = Producer.create({
-  queueUrl: 'https://sqs.ap-northeast-2.amazonaws.com/523139768306/EC2_SQS_ECS',
-  region: 'ap-northeast-2'
+const AWS = require("aws-sdk");
+AWS.config.update({region: 'ap-northeast-2'});
+const credentials = new AWS.SharedIniFileCredentials({
+  profile: "mason",
 });
+var sqs = new AWS.SQS({credentials: credentials, apiVersion: '2012-11-05'});
+var params = {
+ DelaySeconds: 1,
+ MessageBody: "reservation succeeded",
+ QueueUrl: "https://sqs.ap-northeast-2.amazonaws.com/523139768306/EC2_SQS_ECS"
+};
 
-app.use(bodyParser.json())
-app.use(
-  cors({
-    origin: true,
-    credentials: true
-  })
-);
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello june World!')) 
 
-app.post('/api/post/june', async function(req, res){
-    console.log(req.body)
-    await producer.send(JSON.stringify(req.body))
-    res
-      .status(200)
-      .send('message accepted!')
-  });
+app.post('/api/post/june', (req, res) => {
+  sqs.sendMessage(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+    }
+  })
+  res.statusCode(200)
+    .send("message accepted");
+})
   
 app.listen(port, () => {
   console.log(`서버가 ${port}번에서 작동중입니다.`);
