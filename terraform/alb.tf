f50c63e0fd630ea835401219020151555a819e42
crowss-nest <wise.arcn@gmail.com>
2022-06-01 16:08:40 +0900

    alb-ag terraform arranged

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -15,6 +15,17 @@ resource "aws_lb_target_group" "project4_tg" {
   port     = "80"
   protocol = "HTTP"
   vpc_id   = aws_vpc.project4_vpc.id
+  health_check {
+    enabled         = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 30
+    matcher = 200
+    path = "/index.html"
+    port = "traffic-port"
+    protocol = "HTTP"
+    timeout = 5
+  }
 }
 
 resource "aws_lb_listener" "alb-listener" {
